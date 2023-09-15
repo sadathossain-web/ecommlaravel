7967c5e2d247354d2cefa36d52b93a0556a4e1a5
shimpux3 <shimpux3@gmail.com>
2023-09-15 07:26:22 +0100

    updated

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -230,48 +230,23 @@
                     </div>
                 </div>
                 <div class="col-lg-9 col-md-7">
-                    <div class="product__discount">
-                        {{-- <div class="section-title product__discount__title">
-                            <h2>Sale Off</h2>
-                        </div> --}}
-                        <div class="row">
-                            <div class="product__discount__slider owl-carousel">
-                                @foreach ($products as $product)
-                                <div class="col-lg-4">
-                                    <div class="product__discount__item">
-                                        <div class="product__discount__item__pic set-bg"
-                                            data-setbg=" {{ asset($product->image_one) }}">
-                                            <div class="product__discount__percent">-20%</div>
-                                            <ul class="product__item__pic__hover">
-                                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="product__discount__item__text">
-                                            <span>{{ $product->product_code }}</span>
-                                            <h5><a href="#">{{ $product->product_name }}</a></h5>
-                                            <div class="product__item__price">${{ $product->price }} <span></span></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
+
                     <div class="row">
                         @foreach ($productsp as $product)
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
                                 <div class="product__item__pic set-bg" data-setbg=" {{ asset($product->image_one) }}">
                                     <ul class="product__item__pic__hover">
-                                        <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                                        {{-- <li><a href="{{ url('add/to-wishlist/'.$product->id) }}"><i class="fa fa-heart"></i></a></li>
+                                        <li><a href=""><i class="fa fa-retweet"></i></a></li>
+                                        <form action="{{ url('add/to-cart/'.$product->id) }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" name="price" value="{{ $product->price }}">
+                                           <li><button type="submit" ><i class="fa fa-shopping-cart"></i></button></li> --}}
                                     </ul>
                                 </div>
                                 <div class="product__item__text">
-                                    <h6><a href="#">{{ $product->product_name }}</a></h6>
+                                    <h6><a href="{{ url('proudct/details/'.$product->id) }}">{{ $product->product_name }}</a></h6>
                                     <h5>${{ $product->price }}</h5>
                                 </div>
                             </div>
